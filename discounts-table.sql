79f33d4107a503d2b26b6f475ab8d91d8bb14f9d
LiquArioR <kuznetsov2026@mail.ru>
2026-09-22 14:05:51 +0700

    Add DevOps portfolio from completed coursework

---
+++ discounts-table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE person_discounts (
+    id BIGINT PRIMARY KEY,
+    person_id BIGINT NOT NULL,
+    pizzeria_id BIGINT NOT NULL,
+    discount NUMERIC(5,2) DEFAULT 0,
+    CONSTRAINT fk_person_discounts_person_id FOREIGN KEY (person_id) REFERENCES person(id),
+    CONSTRAINT fk_person_discounts_pizzeria_id FOREIGN KEY (pizzeria_id) REFERENCES pizzeria(id)
+);
+
+SELECT count(*) = 1 AS check
+FROM pg_tables
+WHERE tablename = 'person_discounts';
